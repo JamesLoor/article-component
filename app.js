f23bab6a76bceec02811c369bfbd4bb3a573477d
James <mendozajames02@hotmail.com>
2021-10-27 16:45:42 -0500

    Finished article component

---
+++ app.js
@@ -0,0 +1,13 @@
+// console.log('Hey!')
+
+
+// const $iconShare = document.getElementById('icon-share-id')
+// const $tooltip = document.getElementById('tooltip')
+
+// $iconShare.addEventListener('mouseenter', () => {
+//   $tooltip.classList.add('active')
+// })
+
+// $iconShare.addEventListener('mouseleave', () => {
+//   $tooltip.classList.remove('active')
+// })
